f826fe358f0230f4abadea8c977fa1f144fd3aff
ClairCrest <topwarapol8850@gmail.com>
2024-11-13 15:16:05 +0000

    Added Test cases

--- test_zoo.py
+++ test_zoo.py
@@ -5,9 +5,20 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+    def test_child_ticket_price_C1b1(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "ERROR") # 1 Fault fixed
+
+    def test_child_ticket_price_C1b2(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)# 2 Fault fixed
+    
+    def test_child_ticket_price_C1b3(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100) # 3 Fault fixed
+
+    def test_child_ticket_price_C1b4(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150) # 4 Fault fixed
+
+    def test_child_ticket_price_C1b5(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100) 
     # Add your additional test cases here.
 
 if __name__ == '__main__':
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: #Add '=' before 'age' ///Fault 2
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: #Add '=' after '<' sign before 20 ///Fault 3
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: #Add '=' after '<' sign after 21 ///Fault 4
             return 150
         elif age >= 60:
             return 100
+        else:
+            return "ERROR" #Case age < 0 ///Fault 1
